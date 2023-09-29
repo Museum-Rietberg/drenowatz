22ae1bc8bf830064c7dad91619c0195d06358524
Rae Knowler <rae.knowler@liip.ch>
2023-09-29 21:16:27 +0200

    Map seals onto people overview data

--- process/generate_people_data.py
+++ process/generate_people_data.py
@@ -5,7 +5,7 @@ PEOPLE_OVERVIEW_PATH = os.path.join("..", "ui", "public", "people_overview.json"
 TYPES_OF_CREATION = ['', 'Aufschrift', 'Kalligrafie', 'Provenienzmerkmal', 'Kolophon', 'Malerei']
 
 
-def _add_work(creator_works, creator, inventarnummer, creation_type, original_creator=False):
+def _add_work(people_overview, creator, inventarnummer, creation_type, original_creator=False):
     if creation_type == "":
         if original_creator is True:
             # We are assuming the first listed creator is the painter.
@@ -14,13 +14,21 @@ def _add_work(creator_works, creator, inventarnummer, creation_type, original_cr
             # This applies to just one 'creator' of a work.
             creation_type = "im Stil von"
 
-    if creator in list(creator_works.keys()):
-        if creation_type in creator_works[creator]:
-            creator_works[creator][creation_type].append(inventarnummer)
+    if creator in people_overview:
+        if creation_type in people_overview[creator]:
+            people_overview[creator][creation_type].append(inventarnummer)
         else:
-            creator_works[creator][creation_type] = [inventarnummer]
+            people_overview[creator][creation_type] = [inventarnummer]
     else:
-        creator_works[creator] = {creation_type: [inventarnummer]}
+        people_overview[creator] = {creation_type: [inventarnummer]}
+
+
+def _add_seal(people_overview, person, inventarnummer):
+    if person in people_overview:
+        if "seals" in people_overview[person]:
+            people_overview[person]["seals"].append(inventarnummer)
+        else:
+            people_overview[person]["seals"] = [inventarnummer]
 
 
 if __name__ == "__main__":
@@ -31,6 +39,10 @@ if __name__ == "__main__":
     with open(os.path.join("..", "data", "MRZ_Kunstwerke.json")) as f:
         kunstwerke = json.loads(f.read())
     works = {work["Inventarnummer"]: work for work in kunstwerke}
+
+    with open(os.path.join("..", "data", "MRZ_PMs.json")) as f:
+        pms = json.loads(f.read())
+    seals = {seal["Inventarnummer"]: seal for seal in pms}
 
     people_overview = {}
     types_of_creation = []
@@ -61,6 +73,18 @@ if __name__ == "__main__":
         people_overview[creator_id]["name"] = people[creator_id]["AnzeigeName"]
 
     # Map seals to creators
+    for seal_inventarnummer in seals:
+        seal = seals[seal_inventarnummer]
+        if len(seal["Personen"]) > 0:
+            _add_seal(
+                people_overview,
+                seal["Personen"][0]["Personen_ID"],
+                seal_inventarnummer
+            )
+
+    # Todo: Add data from Provenienzmerkmale field on works in MRZ_Kunstwerke.json
+    # Some works have the seal of a creator listed as a PM, but that creator is not
+    # listed as a creator on the work.
 
     with open(os.path.join(PEOPLE_OVERVIEW_PATH), "w") as f:
         f.write(json.dumps(people_overview, indent=4))
