9de7ff25560b527ba4c7357051d28c90660c29fa
Kevin Bieri <bierike@gmail.com>
2023-09-30 15:26:48 +0200

    Fix person overview

--- ui/src/components/PersonCard.vue
+++ ui/src/components/PersonCard.vue
@@ -26,6 +26,11 @@ export default {
       required: true,
     },
   },
+  computed: {
+    thumbnailPath() {
+      return "https://fra1.digitaloceanspaces.com/drenowatz" + this.thumbnail;
+    },
+  },
 };
 </script>
 
@@ -34,25 +39,21 @@ export default {
     :to="{ name: 'personview', params: { id } }"
     class="max-w-sm rounded bg-white overflow-hidden"
   >
-    <img class="w-full" :src="thumbnail" />
+    <img class="w-full h-36 object-cover" :src="thumbnailPath" />
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{{ title }}</div>
     </div>
     <div class="px-6 pt-4 pb-2">
-      <span
-        class="block px-3 py-1 text-sm font-bold text-gray-700 mr-2 mb-2"
-        >{{ name }}</span
-      >
-      <span
-        class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
+      <span class="block px-3 py-1 text-sm font-bold text-gray-700 mr-2 mb-2">{{
+        name
+      }}</span>
+      <span class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
         >Painted {{ number_works_painted }} works</span
       >
-      <span
-        class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
+      <span class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
         >Placed seal on {{ number_works_sealed }} works</span
       >
-      <span
-        class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
+      <span class="inline-block px-3 py-1 text-sm text-gray-700 mr-2 mb-2"
         >Used {{ number_seals }} different seals</span
       >
     </div>
