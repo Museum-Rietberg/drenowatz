6d0f1efb2ca73d588f8669c692b5d764d25587bf
Rae Knowler <rae.knowler@liip.ch>
2023-09-30 14:58:43 +0200

    Skeleton person view

---
+++ ui/src/views/PersonView.vue
@@ -0,0 +1,18 @@
+<script>
+import DefaultLayout from "../components/DefaultLayout.vue";
+import {S3_BUCKET, S3_ENDPOINT} from "../config";
+export default {
+  components: {DefaultLayout},
+  computed: {
+    data() {
+      return `${S3_ENDPOINT}/${S3_BUCKET}/people/${this.$route.params.id}.json`;
+    },
+  },
+};
+</script>
+
+<template>
+  <DefaultLayout narrow>
+    {{ data }}
+  </DefaultLayout>
+</template>
